5ec9c029899f2494b429e3abd57e1d4ea981c70d
kikookraft <kikookraft.teamcraft@gmail.com>
2022-09-03 20:18:50 +0200

    cleaning code + adding comment

--- screen/screen.ino
+++ screen/screen.ino
@@ -1,3 +1,9 @@
+// by kikookraft with the help of:
+// https://arduinojson.org/v6/example/parser/
+// https://github.com/lakshanthad/Wio_Terminal_Classroom_Arduino/tree/main/Classroom%202
+
+
+// Find the details about library in the readme.md file
 #include <TFT_eSPI.h> // Graphics and font library for ILI9341 driver chip
 #include <SPI.h>
 #include <ArduinoJson.h>
@@ -9,9 +15,7 @@ TFT_eSPI tft = TFT_eSPI();  // Invoke library
 //                                    Setup
 //====================================================================================
 
-void setup(void) {
-    int height = 240;
-    int width = 320;
+void setup(void) { // initiate the screen
     Serial.begin(9600);
     tft.setTextColor(TFT_YELLOW);
     tft.setTextSize(2);
@@ -37,13 +41,16 @@ void loop() {
     if (err == DeserializationError::Ok) 
     {
       drawscreen();
-      int nb = 8;
-      for(int i=1; i<nb+1; i++) {
+      int nb = 8; // number of line used
+
+
+      for(int i=1; i<nb+1; i++) { 
+        //for each item: get his name, amount, ... and call the function to draw on screen
         String index = String(i);
-        const char* item = doc[index]["item"];
-        int prod = doc[index]["current"];
-        float effi = doc[index]["effi"];
-        int C = doc[index]["prod"]["C"];
+        const char* item = doc[index]["item"]; // name of the item
+        int prod = doc[index]["current"]; // total production of this item
+        float effi = doc[index]["effi"]; // efficiency %age
+        int C = doc[index]["prod"]["C"]; // Cosumption of this item
         put_item(i,item, effi, prod, C);
       }
       
@@ -63,23 +70,32 @@ void drawscreen() {
   int height = 240;
   int width = 320;
   int line_number=3;
-  for(int i=1; i<line_number; i++) {
+
+  // drawing the verticals lines of the table
+  for(int i=1; i<line_number; i++) { 
     int posx = width/line_number*i;
     tft.drawLine(posx,0,posx,height,tft.color565(169,120,0));
   }
   int space = 25;
+
+  // drawing the horizontal lines of the table
   for(int i=1;space*i<height;i++) {
     tft.drawLine(0,i*space,width,i*space,tft.color565(169,120,0));
   }
+
   tft.setTextColor(TFT_YELLOW);
   tft.setTextSize(2);
   tft.setTextDatum(TL_DATUM);
-  tft.drawString("Produits", 7, 4);
-  tft.drawString("% effi", 126, 4);
-  tft.drawString("qte prod", 221, 4);
+
+  // if you change the text below, you may need to adjust the position
+  tft.drawString("Products", 7, 4); // item name text
+  tft.drawString("% effi", 126, 4); // efficiency text
+  tft.drawString("nb prod", 221, 4); // total production text
+  // example: tft.drawString("text you want", pos x, pos y);
 }
 
 void put_item(int line, const char* item, float effi, int prod,int C) {
+  // put the item and its data in the screen
   int height = 240;
   int width = 320;
   int space = 25;
@@ -87,22 +103,31 @@ void put_item(int line, const char* item, float effi, int prod,int C) {
   tft.setTextDatum(MC_DATUM);
   tft.setTextSize(2);
   int posy = space*line+(space/2);
+
+  // for each column, set the position 
   for(int i=0; i<3;i++) {
     int posx = width/3*i+(width/3)/2;
-    if (i==0) {
-      if (strlen(item)>=10) {tft.setTextSize(1);}
-      if (strlen(item)<=2) {item = "N/A";}
+
+    if (i==0) { // setting up the item name
+      if (strlen(item)>=10) {tft.setTextSize(1);} // if text is longer than 10 char, size it down
+      if (strlen(item)<=2) {item = "N/A";} // if no name avaliable set text to N/A
       tft.drawString(item, posx, posy);
       tft.setTextSize(2);
-      }
-    if (i==1) {
+    }
+
+    if (i==1) { // put the efficiency number with a color system
+      // if %age if near 0 the color will be more red
+      // if %age is near 100 the color will be more green
+      // example: if the number is 75, there will be 75% of green and 25% of red in the color 
       tft.setTextColor(tft.color565(255*(1-(effi/100)),255*(effi/100),20));
       tft.drawNumber(effi, posx, posy);
-      }
-    if (i==2) {
+    }
+
+    if (i==2) { // put the total production number
+      // if this item is more consummed than created, show in red or else in blue
       if (C>prod) {tft.setTextColor(tft.color565(255,20,20));}
       else {tft.setTextColor(tft.color565(100,100,255));}
       tft.drawNumber(prod, posx, posy);
-      }
+    }
   }
 }
